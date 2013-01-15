342cdde532ac3012289fda0c0bcc5dae5e435929
Paul van Brouwershaven <paul.vanbrouwerhaven@globalsign.com>
2013-01-15 13:00:11 +0100

    Update for FreeBSD

--- ext/DirectAdmin/scripts/install.sh
+++ ext/DirectAdmin/scripts/install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+OS=`uname`
 
 if [ -z $DOCUMENT_ROOT ]
 then
 	export DOCUMENT_ROOT='/usr/local/directadmin/plugins/OneClickSSL/scripts'
 fi
 
-cd $DOCUMENT_ROOT;
+cd $DOCUMENT_ROOT
 cd ..
 
 if [ ! -f etc/oneclick.conf ];
@@ -13,22 +14,26 @@ then
 	/bin/cp etc/oneclick.conf_default etc/oneclick.conf
 fi
 
-/bin/chmod 755 -R $DOCUMENT_ROOT/../
-/bin/chown diradmin:diradmin -R $DOCUMENT_ROOT/../
+if [ "$OS" = "FreeBSD" ]; then
+        CHOWN=/usr/sbin/chown
+else
+        CHOWN=/bin/chown
+fi
+
+$CHOWN -R diradmin:diradmin $DOCUMENT_ROOT/../
+/bin/chmod -R 755 $DOCUMENT_ROOT/../
 
 /bin/mkdir -p tmp
 
-/bin/chmod 777 -R tmp
-/bin/chmod 777 -R etc
+/bin/chmod -R 777 tmp
+/bin/chmod -R 777 etc
 
 /bin/chmod 644 plugin.conf
 /bin/chmod 644 hooks/*
 /bin/chmod 644 images/*
 
 /bin/echo '<p>GlobalSign OneClickSSL Plugin Installed!</p>';
-
 /bin/echo '<p><a href="http://www.globalsign.com/ssl/oneclickssl/directadmin/" target="_blank"> Click here to visit our website for more information and support</a></p>';
-
 /bin/echo '<p><strong><a href="/CMD_PLUGINS_ADMIN/OneClickSSL">Please check the configuration of the plugin now!</a></strong></p>';
 
 exit 0;
