0bdca37756f9bb3e0a8fcd5a071b7ceb737c5ed9
Paul van Brouwershaven <paul@vanbrouwershaven.com>
2013-05-15 14:06:26 +0200

    Update apache.php

--- ext/Apache/apache.php
+++ ext/Apache/apache.php
@@ -316,15 +316,15 @@ class ApacheOneClick implements OneClickSSLPlugin
      */     
     public function backup()
     {
- 		if (is_file(CERTDIR . $this->_domain .'.key')) {
-			if (@copy(CERTDIR . $this->_domain .'.*', CERTDIR .'backup/')) {
-				return true;
-         	} else {
-				return false;
-			}
-		} else {
-			return true;
-		}
+        if (is_file(CERTDIR . $this->_domain .'.key')) {
+            if (@copy(CERTDIR . $this->_domain .'.*', CERTDIR .'backup/')) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
     }
 	
     /**
