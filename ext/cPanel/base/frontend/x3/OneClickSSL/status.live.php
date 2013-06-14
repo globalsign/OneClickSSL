64cbfe9e003d54d6446c12ca56194ac30794d6af
Paul van Brouwershaven <paul.vanbrouwerhaven@globalsign.com>
2013-06-14 07:49:05 +0200

    Loading cPanel class to overcome 'timeout'

--- ext/cPanel/base/frontend/x3/OneClickSSL/status.live.php
+++ ext/cPanel/base/frontend/x3/OneClickSSL/status.live.php
@@ -34,6 +34,15 @@
 * @version    $Id$
 * @link       http://www.globalsign.com/ssl/oneclickssl/
 */
+
+/**
+ * cPanel library
+ 
+ */
+include("/usr/local/cpanel/php/cpanel.php");
+
+$cpanel = new CPANEL();
+
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 header("Cache-Control: no-cache");
